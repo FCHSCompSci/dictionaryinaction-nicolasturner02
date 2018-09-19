da0a6262222589001c78d4c8a78a5b8176b01988
Turner <nicolas.turner@stu.jefferson.kyschools.us>
2018-09-19 08:30:28 -0400

    i redone the entire thing

--- league champs.py
+++ league champs.py
@@ -1,21 +1,15 @@
 import sys
-Shen="super Tank"
-Nautilus="BIG TANK"
-Aatrox="was great but got reworked"
-toplane="upper part of the map"
-trashcan="garbage"
-champs={
-    'Shen':toplane,
-        'Nautilus':toplane ,
-            'Aatrox':trashcan,
+champ={
+    "exp": 150000,
+    "kills": 0,
+    "MASTERY": 7,
+    "deaths": 0,
     }
-def play_game():
-    input("do you want to play Shen,Nautilus,or Aatrox(hint it must be Shen)")
-    if input != Shen:
-        return (champs)
-    else:
-        print=("you lose")
-        sys.exit
-          
+toplane_heavy=("Shen")
+def shenden():
+    shen=input("would you like to play the shenden if so type shen if not type what you want")
+    if shen == Shen:
+        return champ
+
 
 
